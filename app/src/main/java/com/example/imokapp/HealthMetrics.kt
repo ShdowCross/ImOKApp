de1823da3c565710849f9169f24b2dbed1fa8e2d
siinyann <sinyanchong4@gmail.com>
2023-07-09 16:10:53 +0800

    Added intents

--- app/src/main/java/com/example/imokapp/HealthMetrics.kt
+++ app/src/main/java/com/example/imokapp/HealthMetrics.kt
@@ -1,11 +1,24 @@
 package com.example.imokapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_health_metrics.*
 
 class HealthMetrics : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_health_metrics)
+
+        manualInputBtn.setOnClickListener() {
+            var myIntent = Intent(this, ManualInput::class.java)
+            startActivity(myIntent)
+        }
+
+        graphBtn.setOnClickListener() {
+            var myIntent = Intent(this, Graph::class.java)
+            startActivity(myIntent)
+        }
     }
 }
--- app/src/main/java/com/example/imokapp/ManualInput.kt
+++ app/src/main/java/com/example/imokapp/ManualInput.kt
@@ -1,11 +1,18 @@
 package com.example.imokapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_manual_input.*
 
 class ManualInput : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manual_input)
+
+        cancelBtn.setOnClickListener() {
+            var myIntent = Intent(this, HealthMetrics::class.java)
+            startActivity(myIntent)
+        }
     }
 }
