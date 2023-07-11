a23d4775f84bc182008581ec681f806cb8d0d20c
siinyann <sinyanchong4@gmail.com>
2023-07-11 23:00:31 +0800

    Updated intents

--- app/src/main/java/com/example/imokapp/HealthMetrics.kt
+++ app/src/main/java/com/example/imokapp/HealthMetrics.kt
@@ -3,37 +3,48 @@ package com.example.imokapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat.startActivity
 import kotlinx.android.synthetic.main.activity_health_metrics.*
 
 class HealthMetrics : AppCompatActivity() {
+
+//    val HEALTH_METRICS_RESULT_CODE = 1;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_health_metrics)
 
-        manualInputBtn.setOnClickListener() {
-            var myIntent = Intent(this, ManualInput::class.java)
-            startActivity(myIntent)
-        }
+        var weightFromManualInput = intent.getStringExtra("enter_weight")
+        var heartRateFromManualInput = intent.getStringExtra("enter_heart_rate")
+        var bloodPressureFromManualInput = intent.getStringExtra("enter_blood_pressure")
 
-        bloodPressureIV.setOnClickListener() {
-            var myIntent = Intent(this, BpGraph::class.java)
-            startActivity(myIntent)
-        }
+        weightKgTV.text = "$weightFromManualInput"
+        hrBPMTV.text = "$heartRateFromManualInput bpm"
+        bpmmHgTV.text = "$bloodPressureFromManualInput mmHg"
 
-        weightIV.setOnClickListener() {
-            var myIntent = Intent(this, WeightGraph::class.java)
-            startActivity(myIntent)
-        }
 
-//        bpGraphBtn.setOnClickListener() {
-//            var myIntent = Intent(this, BpGraph::class.java)
-//            startActivity(myIntent)
-//        }
-//
-//        weightGraphBtn.setOnClickListener() {
-//            var myIntent = Intent(this, WeightGraph::class.java)
-//            startActivity(myIntent)
-//        }
+        var weightForBMI = weightFromManualInput?.toFloat()
+        var heightForBMI = (heightMTV.text as String).toFloat()
+
+        var calculatedBMI = weightForBMI?.div((heightForBMI * heightForBMI))
+
+        bmiMessurementTV.text = String.format("%.2f",calculatedBMI) + " kg/m\u00B2"
+
+    }
+
+    fun manualInput(v:View) {
+        var myIntent = Intent(this, ManualInput::class.java)
+        startActivity(myIntent)
+    }
+
+    fun weightGraph(v:View) {
+        var myIntent = Intent(this, WeightGraph::class.java)
+        startActivity(myIntent)
+    }
+
+    fun BpGraph(v:View) {
+        var myIntent = Intent(this, BpGraph::class.java)
+        startActivity(myIntent)
     }
 }
--- app/src/main/java/com/example/imokapp/ManualInput.kt
+++ app/src/main/java/com/example/imokapp/ManualInput.kt
@@ -3,21 +3,61 @@ package com.example.imokapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_manual_input.*
 
 class ManualInput : AppCompatActivity() {
+
+    val HEALTH_METRICS_RESULT_CODE = 1;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manual_input)
 
-        saveBtn.setOnClickListener() {
-            var myIntent = Intent(this, HealthMetrics::class.java)
-            startActivity(myIntent)
-        }
+//        saveBtn.setOnClickListener() {
+//            var myIntent = Intent(this, HealthMetrics::class.java)
+//            startActivity(myIntent)
+//        }
 
         cancelBtn.setOnClickListener() {
             var myIntent = Intent(this, HealthMetrics::class.java)
             startActivity(myIntent)
         }
+    }
+
+    fun saveManualInput(v: View) {
+
+        var errorExist = false
+
+        // Check for empty fields
+        if (weightET.text.isEmpty()) {
+            weightET.error = "Field empty"
+            errorExist = true
+        }
+
+        if (heartRateET.text.isEmpty()) {
+            heartRateET.error = "Field empty"
+            errorExist = true
+        }
+
+        if (bloodPressureET.text.isEmpty()) {
+            bloodPressureET.error = "Field empty"
+            errorExist = true
+        }
+
+        if (errorExist)
+            return
+            // Convert text to string
+            var weight = weightET.text.toString()
+            var heartRate = heartRateET.text.toString()
+            var bloodPressure = bloodPressureET.text.toString()
+
+            var myIntent = Intent(this, HealthMetrics::class.java)
+            var extras =  Bundle();
+            extras.putString("enter_weight",weight)
+            extras.putString("enter_heart_rate",heartRate)
+            extras.putString("enter_blood_pressure",bloodPressure)
+            myIntent.putExtras(extras)
+            startActivityForResult(myIntent,HEALTH_METRICS_RESULT_CODE)
+
     }
 }
