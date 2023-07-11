401fdd77585b0bfdb7b3c252ba546c603b4ce0a8
ShdowCross <cyxjosh@gmail.com>
2023-07-11 12:37:20 +0800

    images done

--- app/src/main/java/com/example/imokapp/PersonContacts.kt
+++ app/src/main/java/com/example/imokapp/PersonContacts.kt
@@ -6,13 +6,14 @@ import android.content.Context
 import android.database.Cursor
 import android.view.View
 import android.widget.*
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_medical_contacts.*
 import java.util.ArrayList
 
 
 class PersonContacts : AppCompatActivity() {
-    var doctor = arrayListOf("Cardiologist", "Dermatologist", "Neurologist", "Pediatrician", "Surgeon")
-    var relative = arrayListOf("Brother", "Cousin", "Aunt", "Uncle", "Grandmother")
+    var doctor = arrayListOf("Cardiologist", "Dermatologist", "Neurologist", "Pediatrician")
+    var relative = arrayListOf("Brother", "Cousin", "Aunt", "Uncle")
     var doctorAdapter: ArrayAdapter<String>? = null
     var relativeAdapter: ArrayAdapter<String>? = null
 
@@ -20,6 +21,12 @@ class PersonContacts : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_medical_contacts)
         val paths = getPaths()
+        val main1Doctor = "General Doctor"
+        val main1Info = getDoctorInfo(main1Doctor)
+        val main2Doctor = "Mother"
+        val main2Info = getRelativeInfo(main2Doctor)
+        Picasso.get().load(resources.getIdentifier(main1Info.imageUrl,"drawable",packageName))
+        Picasso.get().load(resources.getIdentifier(main2Info.imageUrl,"drawable",packageName))
         hideFields(paths.selectedDoctor)
         hideFields(paths.selectedRelative)
         showFields(paths.unselectedDoctor)
@@ -37,13 +44,15 @@ class PersonContacts : AppCompatActivity() {
         medicalSelectionLeft.setOnClickListener{
             hideFields(paths.unselectedDoctor)
             showFields(paths.selectedDoctor)
-            displayToast("You have selected your main doctor")
             val selectedDoctor = "General Doctor"
+            displayToast("You have selected your main doctor")
             var doctorInfo = getDoctorInfo(selectedDoctor)
+            val imageResourceId = resources.getIdentifier(doctorInfo.imageUrl, "drawable", packageName)
             selectedDoctorNickname.text = selectedDoctor
             selectedDoctorName.text = doctorInfo.name
             selectedDoctorTitle.text = doctorInfo.title
             medicalProfileWebsite.text = doctorInfo.website
+            Picasso.get().load(imageResourceId).into(paths.selectedDoctorProfileImg)
             selectedDoctorEmail.text = doctorInfo.email
             selectedDoctorPhoneNumber.text = doctorInfo.phoneNumber
             selectedDoctorStreet.text = doctorInfo.street
@@ -57,11 +66,13 @@ class PersonContacts : AppCompatActivity() {
             showFields(paths.selectedDoctor)
             val selectedDoctor = parent?.adapter?.getItem(position).toString()
             val doctorInfo = getDoctorInfo(selectedDoctor)
+            val imageResourceId = resources.getIdentifier(doctorInfo.imageUrl, "drawable", packageName)
             displayToast("You have selected " + parent?.adapter?.getItem(position) + " of doctor")
             selectedDoctorNickname.text = selectedDoctor
             selectedDoctorName.text = doctorInfo.name
             selectedDoctorTitle.text = doctorInfo.title
             medicalProfileWebsite.text = doctorInfo.website
+            Picasso.get().load(imageResourceId).into(paths.selectedDoctorProfileImg)
             selectedDoctorEmail.text = doctorInfo.email
             selectedDoctorPhoneNumber.text = doctorInfo.phoneNumber
             selectedDoctorStreet.text = doctorInfo.street
@@ -73,10 +84,12 @@ class PersonContacts : AppCompatActivity() {
             showFields(paths.selectedRelative)
             val selectedRelative = "Mother"
             val relativeInfo = getRelativeInfo(selectedRelative)
+            val imageResourceId = resources.getIdentifier(relativeInfo.imageUrl, "drawable", packageName)
             selectedRelativeNickname.text = selectedRelative
             selectedRelativeName.text = relativeInfo.name
             selectedRelativeNRIC.text = relativeInfo.nric
             selectedRelativeTitle.text = relativeInfo.title
+            Picasso.get().load(imageResourceId).into(paths.selectedRelativeProfileImg)
             selectedRelativeEmail.text = relativeInfo.email
             selectedRelativePhoneNumber.text = relativeInfo.phoneNumber
             selectedRelativeStreet.text = relativeInfo.street
@@ -91,10 +104,12 @@ class PersonContacts : AppCompatActivity() {
             showFields(paths.selectedRelative)
             val selectedRelative = parent?.adapter?.getItem(position).toString()
             val relativeInfo = getRelativeInfo(selectedRelative)
+            val imageResourceId = resources.getIdentifier(relativeInfo.imageUrl, "drawable", packageName)
             selectedRelativeNickname.text = selectedRelative
             selectedRelativeName.text = relativeInfo.name
             selectedRelativeNRIC.text = relativeInfo.nric
             selectedRelativeTitle.text = relativeInfo.title
+            Picasso.get().load(imageResourceId).into(paths.selectedRelativeProfileImg)
             selectedRelativeEmail.text = relativeInfo.email
             selectedRelativePhoneNumber.text = relativeInfo.phoneNumber
             selectedRelativeStreet.text = relativeInfo.street
@@ -106,9 +121,10 @@ class PersonContacts : AppCompatActivity() {
     private fun getDoctorInfo(doctorName: String): DoctorInfo {
         return when (doctorName) {
             "General Doctor" -> DoctorInfo(
-                name = "Dr. John Smith",
+                name = "Dr. Smith",
                 title = "General Doctor",
                 website = "https://example.com/doctors/johnsmith",
+                imageUrl = "doctor1",
                 email = "johnsmith@example.com",
                 phoneNumber = "+65 12345678",
                 street = "123 Main Street",
@@ -116,9 +132,10 @@ class PersonContacts : AppCompatActivity() {
                 postal = "123456"
             )
             "Cardiologist" -> DoctorInfo(
-                name = "Dr. John",
+                name = "Dr. Don",
                 title = "Cardiologist",
                 website = "www.cardiologyclinic.com",
+                imageUrl = "doctor5",
                 email = "drjohn@example.com",
                 phoneNumber = "+1234567890",
                 street = "456 Heart Avenue",
@@ -126,9 +143,10 @@ class PersonContacts : AppCompatActivity() {
                 postal = "67890"
             )
             "Dermatologist" -> DoctorInfo(
-                name = "Dr. Emily",
+                name = "Dr. Perkins",
                 title = "Dermatologist",
                 website = "www.dermatologyclinic.com",
+                imageUrl = "doctor3",
                 email = "dremily@example.com",
                 phoneNumber = "+0987654321",
                 street = "789 Skin Street",
@@ -136,9 +154,10 @@ class PersonContacts : AppCompatActivity() {
                 postal = "54321"
             )
             "Neurologist" -> DoctorInfo(
-                name = "Dr. Michael",
+                name = "Dr. Nash",
                 title = "Neurologist",
                 website = "www.neurologyclinic.com",
+                imageUrl = "doctor2",
                 email = "drmichael@example.com",
                 phoneNumber = "+1112223333",
                 street = "321 Brain Avenue",
@@ -146,29 +165,21 @@ class PersonContacts : AppCompatActivity() {
                 postal = "98765"
             )
             "Pediatrician" -> DoctorInfo(
-                name = "Dr. Sarah",
+                name = "Dr. Phua",
                 title = "Pediatrician",
                 website = "www.pediatricsclinic.com",
+                imageUrl = "doctor4",
                 email = "drsarah@example.com",
                 phoneNumber = "+4445556666",
                 street = "234 Kids Street",
                 unit = "Unit 1",
                 postal = "34567"
             )
-            "Surgeon" -> DoctorInfo(
-                name = "Dr. David",
-                title = "Surgeon",
-                website = "www.surgeryclinic.com",
-                email = "drdavid@example.com",
-                phoneNumber = "+7778889999",
-                street = "876 Operation Avenue",
-                unit = "Unit 7",
-                postal = "12345"
-            )
             "Gastroenterologist" -> DoctorInfo(
-                name = "Dr. Samantha",
+                name = "Dr. Theresa",
                 title = "Gastroenterologist",
                 website = "www.gastroclinic.com",
+                imageUrl = "doctor6",
                 email = "drsamantha@example.com",
                 phoneNumber = "+2223334444",
                 street = "567 Stomach Street",
@@ -179,6 +190,7 @@ class PersonContacts : AppCompatActivity() {
                 name = "Unknown Doctor",
                 title = "Unknown Title",
                 website = "",
+                imageUrl = "",
                 email = "",
                 phoneNumber = "",
                 street = "",
@@ -190,69 +202,65 @@ class PersonContacts : AppCompatActivity() {
     private fun getRelativeInfo(relativeName: String): RelativeInfo {
         return when (relativeName) {
             "Mother" -> RelativeInfo(
-                name = "Jane Smith",
+                name = "Elderly Mother",
                 nric = "S9876543B",
                 title = "Mother",
-                email = "janesmith@example.com",
+                imageUrl = "janedoh",
+                email = "elderlymother@example.com",
                 phoneNumber = "+65 87654321",
                 street = "123 Main Street",
                 unit = "#12-34",
                 postal = "654321"
             )
             "Brother" -> RelativeInfo(
-                name = "John",
+                name = "Supportive Brother",
                 nric = "1234567890",
                 title = "Brother",
-                email = "john@example.com",
+                imageUrl = "brother",
+                email = "supportivebrother@example.com",
                 phoneNumber = "+1234567890",
                 street = "123 Brother Street",
                 unit = "Unit 1",
                 postal = "12345"
             )
             "Cousin" -> RelativeInfo(
-                name = "Emily",
+                name = "Caring Cousin",
                 nric = "0987654321",
                 title = "Cousin",
-                email = "emily@example.com",
+                imageUrl = "cousin",
+                email = "caringcousin@example.com",
                 phoneNumber = "+0987654321",
                 street = "456 Cousin Street",
                 unit = "Unit 2",
                 postal = "54321"
             )
             "Aunt" -> RelativeInfo(
-                name = "Samantha",
+                name = "Supportive Aunt",
                 nric = "6789012345",
                 title = "Aunt",
-                email = "samantha@example.com",
+                imageUrl = "aunt",
+                email = "supportiveaunt@example.com",
                 phoneNumber = "+1112223333",
                 street = "789 Aunt Street",
                 unit = "Unit 3",
                 postal = "67890"
             )
             "Uncle" -> RelativeInfo(
-                name = "Michael",
+                name = "Helpful Uncle",
                 nric = "5432109876",
                 title = "Uncle",
-                email = "michael@example.com",
+                imageUrl = "uncle",
+                email = "helpfuluncle@example.com",
                 phoneNumber = "+4445556666",
                 street = "234 Uncle Street",
                 unit = "Unit 4",
                 postal = "34567"
             )
-            "Grandmother" -> RelativeInfo(
-                name = "Sarah",
-                nric = "9876543210",
-                title = "Grandmother",
-                email = "sarah@example.com",
-                phoneNumber = "+7778889999",
-                street = "567 Grandmother Street",
-                unit = "Unit 5",
-                postal = "98765"
-            )
             else -> RelativeInfo(
                 name = "Unknown Relative",
                 nric = "",
                 title = "",
+                imageUrl = "",
                 email = "",
                 phoneNumber = "",
                 street = "",
@@ -276,27 +284,30 @@ class PersonContacts : AppCompatActivity() {
     fun displayToast(message:String){
         Toast.makeText(this,message, Toast.LENGTH_LONG).show()
     }
-    data class Paths(
+    class Paths(
         val selectedDoctor: LinearLayout,
         val selectedRelative: LinearLayout,
         val unselectedDoctor: LinearLayout,
-        val unselectedRelative: LinearLayout
+        val unselectedRelative: LinearLayout,
+        val selectedDoctorProfileImg: ImageView,
+        val selectedRelativeProfileImg: ImageView
     )
-
     fun getPaths(): Paths {
         val selectedDoctor = findViewById<LinearLayout>(R.id.selectedMedical)
         val selectedRelative = findViewById<LinearLayout>(R.id.selectedRelative)
         val unselectedDoctor = findViewById<LinearLayout>(R.id.unselectedDoctorLL)
         val unselectedRelative = findViewById<LinearLayout>(R.id.unselectedRelativeLL)
-        return Paths(selectedDoctor, selectedRelative, unselectedDoctor, unselectedRelative)
+        val selectedDoctorProfileImg = findViewById<ImageView>(R.id.chosenMedicalProfileImg)
+        val selectedRelativeProfileImg = findViewById<ImageView>(R.id.chosenRelativeProfileImg)
+        return Paths(selectedDoctor, selectedRelative, unselectedDoctor, unselectedRelative, selectedDoctorProfileImg, selectedRelativeProfileImg)
     }
-
 }
 
 data class DoctorInfo(
     val name: String,
     val title: String,
     val website: String,
+    val imageUrl : String,
     val email: String,
     val phoneNumber: String,
     val street: String,
@@ -307,6 +318,7 @@ data class RelativeInfo(
     val name: String,
     val nric: String,
     val title: String,
+    val imageUrl : String,
     val email: String,
     val phoneNumber: String,
     val street: String,
